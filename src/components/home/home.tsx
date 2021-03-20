5574b0ccc2cefcd1966f33366f92282754a17f61
Tchaka Spierings <tchaka.spierings@gmail.com>
2021-03-20 17:32:02 +0100

    Add start screen

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
 import './home.scss';
+import background from '../../assets/background.png'
+import play from '../../assets/play.png'
 
 export class Home extends React.Component<{}, {}> {
 
   render() {
-    return <div className="content">
-      <header>
-        <h1>New app</h1>
-      </header>
-      <section>
-        Blast it
-      </section>
-      <footer>
-        <a href="/">Home</a>
-      </footer>
+    return <div className="content" style={{
+      backgroundImage: `url(${background})`
+    }}>
+      <div>Druk op de groene knop om jouw perfecte hond te creëren</div>
+      <button style={{backgroundImage: `url(${play})`}}/>
     </div>;
   }
 }
