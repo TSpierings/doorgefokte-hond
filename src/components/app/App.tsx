86543a54d0670eec1b86da05f7b5f3761c833055
Tchaka Spierings <tchaka.spierings@gmail.com>
2021-03-20 16:22:22 +0100

    Remove unnecessary dependencies

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,30 +6,22 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { rootReducer } from '../../store/index';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 
 import { Home } from '../home/home';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
 
-          <Route path="*">
-            <Redirect to="/"/>
-          </Route>
-        </Switch>
-      </Router>
-    </Provider>
+        <Route path="*">
+          <Redirect to="/"/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/store/index.ts
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from 'redux';
-
-export const rootReducer = combineReducers({ /* Add Some reducers right here */});
-
-export type RootState = ReturnType<typeof rootReducer>;
