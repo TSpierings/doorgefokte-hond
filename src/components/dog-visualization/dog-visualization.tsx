b0e349e23312f2e5a92e7570d25e72f5a16b7497
Tchaka Spierings <tchaka.spierings@gmail.com>
2021-03-31 13:15:16 +0200

    Implement draw order and part highlighting

--- src/components/dog-visualization/dog-visualization.tsx
+++ src/components/dog-visualization/dog-visualization.tsx
@@ -12,11 +12,12 @@ interface DogVisualizationProps {
 export class DogVisualization extends React.Component<DogVisualizationProps, {}> {
   private canvasRef: React.RefObject<HTMLCanvasElement>;
   private locationsByPhase = [
-    {x: 0, y: 0},
-    {x: 455, y: 350},
-    {x: 650, y: 650},
-    {x: 2275, y: 455}
+    { x: 0, y: 0 },
+    { x: 455, y: 350 },
+    { x: 650, y: 650 },
+    { x: 2275, y: 455 }
   ];
+  private drawOrder = [0, 2, 3, 1];
 
   constructor(props: any) {
     super(props);
@@ -28,21 +29,24 @@ export class DogVisualization extends React.Component<DogVisualizationProps, {}>
     const canvas = this.canvasRef.current as HTMLCanvasElement;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-    const images = partsByPhase.map((part, index) => {
+    const images = this.drawOrder.map(item => {
+      const part = partsByPhase[item];
       return new Promise(resolve => {
         const partImage = new Image();
-        partImage.src = part[this.props.selectedParts[index]]
+        partImage.src = part[this.props.selectedParts[item]]
         partImage.onload = () => resolve(partImage);
       });
     });
 
-    const foo = await Promise.all(images);
+    const loadedImages = await Promise.all(images);
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    foo.forEach((item, index) => {
-      const coords = this.locationsByPhase[index];
-      ctx.drawImage(item as CanvasImageSource, 800 + coords.x, 250 + coords.y);
+    this.drawOrder.forEach((item, index) => {
+      const image = loadedImages[index]
+      const coords = this.locationsByPhase[item];
+      ctx.filter = this.props.phase === item ? 'contrast(0.8) brightness(1.2)' : 'contrast(1) brightness(1)';
+      ctx.drawImage(image as CanvasImageSource, 800 + coords.x, 250 + coords.y);
     });
   }
 
